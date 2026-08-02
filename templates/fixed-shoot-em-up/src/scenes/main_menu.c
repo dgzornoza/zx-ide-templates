838e3b09b3daa4b34d1d71233fffc10b58579e28
dgzornoza <dgzornoza@dgzornoza.com>
2026-08-02 16:50:42 +0000

    feature (menu) creacion menu basico

--- templates/fixed-shoot-em-up/src/scenes/main_menu.c
+++ templates/fixed-shoot-em-up/src/scenes/main_menu.c
@@ -1,6 +1,5 @@
 /*
  * main_menu.c - Main menu scene for ZX-IDE fixed-shoot-em-up template.
- *
  * Runs AFTER splash transitions game_state to STATE_MENU.
  */
 
@@ -13,33 +12,19 @@
 #include "../game_state.h"
 #include "../core/system_definitions.h"
 #include "../core/utils/draw_string_tiles.h"
-#include "../core/utils/sp1_clear_screen.h"
-
-/* -------------------------------------------------------------------------
- * Display geometry
- * ------------------------------------------------------------------------- */
 
 /* Menu entries */
-#define MENU_LEFT_COL 8u
+#define MENU_LEFT_COL 10u
 
 #define MENU_LABEL_0 "0 Jugar"
-#define MENU_LABEL_0_ROW 4u
 #define MENU_LABEL_1 "1 Keyboard"
-#define MENU_LABEL_1_ROW 7u
 #define MENU_LABEL_2 "2 Kempston"
-#define MENU_LABEL_2_ROW 9u
 #define MENU_LABEL_3 "3 Sinclair"
-#define MENU_LABEL_3_ROW 11u
 
 /* Copyright block */
-#define COPYRIGHT_COL 0u
-
-#define COPYRIGHT_LABEL_0 "(C) 2026 David Gonzalez Zornoza"
-#define COPYRIGHT_LABEL_0_ROW 18u
+#define COPYRIGHT_LABEL_0 "(C) 2026 David Gonzalez"
 #define COPYRIGHT_LABEL_1 "fixed-shoot-em-up"
-#define COPYRIGHT_LABEL_1_ROW 20u
-#define COPYRIGHT_LABEL_2 "powered by ZxIde"
-#define COPYRIGHT_LABEL_2_ROW 22u
+#define COPYRIGHT_LABEL_2 "powered by Zx-Ide"
 
 /* -------------------------------------------------------------------------
  * Lifecycle
@@ -50,17 +35,16 @@ void main_menu_scene_init(void) __z88dk_fastcall
     zx_border(PAPER_BLACK);
     zx_cls(PAPER_BLACK);
 
-    /* Menu entries. */
-    draw_string_tiles(MENU_LABEL_0_ROW, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_0);
-    draw_string_tiles(MENU_LABEL_1_ROW, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_1);
-    draw_string_tiles(MENU_LABEL_2_ROW, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_2);
-    draw_string_tiles(MENU_LABEL_3_ROW, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_3);
+    // /* Menu entries. */
+    draw_string_tiles(4u, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_0);
+    draw_string_tiles(7u, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_1);
+    draw_string_tiles(9u, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_2);
+    draw_string_tiles(11u, MENU_LEFT_COL, INK_WHITE | PAPER_BLACK, MENU_LABEL_3);
 
-    /* Copyright block. NOTE: COPYRIGHT_TEXT_2 is 34 chars and exceeds the
-     * 32-column display; the trailing "de" will be clipped off-screen. */
-    draw_string_tiles(COPYRIGHT_LABEL_0_ROW, COPYRIGHT_COL, INK_WHITE | PAPER_BLACK, COPYRIGHT_LABEL_0);
-    draw_string_tiles(COPYRIGHT_LABEL_1_ROW, COPYRIGHT_COL, INK_WHITE | PAPER_BLACK, COPYRIGHT_LABEL_1);
-    draw_string_tiles(COPYRIGHT_LABEL_2_ROW, COPYRIGHT_COL, INK_WHITE | PAPER_BLACK, COPYRIGHT_LABEL_2);
+    // /* Copyright block.*/
+    draw_string_tiles(18u, 0u, INK_WHITE | PAPER_BLACK, COPYRIGHT_LABEL_0);
+    draw_string_tiles(20u, 0u, INK_WHITE | PAPER_BLACK, COPYRIGHT_LABEL_1);
+    draw_string_tiles(22u, 15u, INK_WHITE | PAPER_BLACK, COPYRIGHT_LABEL_2);
 }
 
 void main_menu_scene_update(void) __z88dk_fastcall
